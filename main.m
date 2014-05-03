d294c1d22163d86059ffafdd4cd8282d6a439da3
andybaoxv <changyale123@gmail.com>
2014-05-03 19:25:53 -0400

    commit

--- main.m
+++ main.m
@@ -2,9 +2,17 @@
 clear all, close all, clc;
 
 % Parameter Settings
-lambda = 2.00;
+lambda = 2.0;
 % Number of nearest neighbors in self-tuning spectral clustering
 nn = 7;
+% range of lambda
+v_lambda_range = 0:1:5;
+% Number of latent factors;
+dim_q = 4;
+% tolerance for convergence
+tol = 1e-6;
+% maximum number of iteration
+n_iter_max = 200;
 
 % Load faces dataset
 load img_faces
@@ -23,12 +31,9 @@ load feat_hog
 identity_unique = unique(img_identity);
 Y = zeros(length(img_identity),length(identity_unique));
 for i = 1:length(img_identity)
-    for j = 1:length(identity_unique)
-        if img_identity(i) == identity_unique(j)
-            Y(i,j) = 1;
-        end
-    end
+   Y(i,img_identity(i)) = 1;
 end
+aff_Y = Y*Y';
 
 % Compute similarity matrix from original data
 % Raw data
@@ -44,35 +49,90 @@ aff_gabor = scale_dist3(D,nn);
 D = dist2(feat_hog,feat_hog);
 aff_hog = scale_dist3(D,nn);
 
-% Define new kernel matrix
-A_new = aff_raw-lambda/2*Y*Y';
-
-run_symnmf = 10;
-H_list = {};
-iter_list = zeros(run_symnmf,1);
-obj_list = zeros(run_symnmf,1);
-
-for i = 1:run_symnmf
-    tic
-    i
-    [H_list{i},iter_list(i),obj_list(i)] = symnmf_newton(A_new,4);
-    toc
-end
-for i = 1:run_symnmf
-    if obj_list(i) == min(obj_list)
-        H = H_list{i};
-        i
-        break
+% Define M sources
+affs = {aff_pca,aff_gabor,aff_hog};
+n_sources = length(affs);
+[n_instances,~] = size(aff_raw);
+% Compute dependency between sources
+Q = zeros(n_sources);
+for i = 1:n_sources
+    for j = i:n_sources
+        Q(i,j) = trace(affs{i}*affs{j});
+        Q(j,i) = Q(i,j);
     end
 end
-    
-label_pred = zeros(length(img_pose),1);
-for i = 1:length(img_pose)
-    for j = 1:length(unique(img_pose))
-        if H(i,j) == max(H(i,:))
-            label_pred(i) = j;
+
+for v_lambda_idx = 1:length(v_lambda_range)
+    v_lambda = v_lambda_range(v_lambda_idx);
+    % Initialization of res and res_old
+    res = 2;
+    res_old = 1;
+    % Initialization of low-dimensional representation
+    U = rand(n_instances,dim_q);
+    % Initialization of weights
+    beta = ones(n_sources,1)/n_sources;
+    % Count the number of loops
+    n_iter = 0;
+    while abs(res-res_old)/res_old>tol & n_iter<n_iter_max
+        U_old = U;
+        % Convex combination of kernels of M sources
+        aff = zeros(n_instances);
+        for i = 1:n_sources
+            aff = aff+beta(i)*affs{i};
         end
+        % Find U by Newton like algorithm
+        [U,iter,obj] = symnmf_newton(aff-v_lambda/2*aff_Y,4);
+        beta_old = beta;
+        gamma = zeros(n_sources,1);
+        for i = 1:n_sources
+            gamma(i) = trace(affs{i}*U*U');
+        end
+        G = -eye(n_sources);
+        h = zeros(n_sources,1);
+        A = ones(1,n_sources);
+        b = ones(1);
+        % Find beta by Quadratic Programming
+        beta = quadprog(2*Q,-2*gamma,G,h,A,b);
+        aff_old = zeros(n_instances);
+        for i = 1:n_sources
+            aff_old = aff_old+beta_old(i)*affs{i};
+        end
+        % Compute residue
+        res_old = norm(aff_old-U_old*U_old');
+        res = norm(aff-U*U');
+        n_iter = n_iter+1;
     end
 end
 
-nmi_pose = nmi(label_pred,img_pose);
+% % Define new kernel matrix
+% A_new = aff_gabor-lambda/2*Y*Y';
+% 
+% run_symnmf = 10;
+% H_list = {};
+% iter_list = zeros(run_symnmf,1);
+% obj_list = zeros(run_symnmf,1);
+% 
+% for i = 1:run_symnmf
+%     tic
+%     i
+%     [H_list{i},iter_list(i),obj_list(i)] = symnmf_newton(A_new,4);
+%     toc
+% end
+% for i = 1:run_symnmf
+%     if obj_list(i) == min(obj_list)
+%         H = H_list{i};
+%         i
+%         break
+%     end
+% end
+%     
+% label_pred = zeros(length(img_pose),1);
+% for i = 1:length(img_pose)
+%     for j = 1:length(unique(img_pose))
+%         if H(i,j) == max(H(i,:))
+%             label_pred(i) = j;
+%         end
+%     end
+% end
+% 
+% nmi_pose = nmi(label_pred,img_pose);
